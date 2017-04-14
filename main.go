1b9942d1963798bb097aa86393e8d5a3f0060ae8
Jacopo <dedalusj@gmail.com>
2017-04-14 14:45:37 +1000

    Simpler implementation without certificates

--- main.go
+++ main.go
@@ -1,128 +1,242 @@
-package main  // import "github.com/dedalusj/portainer-endpoints"
+package main
 
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 
-	"github.com/urfave/cli"
 	log "github.com/Sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"github.com/pkg/errors"
+	"github.com/urfave/cli"
+)
+
+const (
+	envPrefix = "PE_"
 )
 
 var version string
 
-func init() {
-	log.SetOutput(os.Stderr)
+type Config struct {
+	Tag      string
+	Output   string
+	Port     int
+	Interval time.Duration
+	Debug    bool
+}
+
+type Endpoint struct {
+	Name string
+	URL  string
+}
+
+type Instance struct {
+	Name string
+	Ip   string
+}
+
+func NewInstance(instance *ec2.Instance) Instance {
+	ip := aws.StringValue(instance.PrivateIpAddress)
+	name := strings.Replace(ip, ".", "-", -1)
+	for _, t := range instance.Tags {
+		if strings.ToLower(aws.StringValue(t.Key)) == "name" {
+			name = strings.ToLower(aws.StringValue(t.Value)) + "-" + name
+		}
+	}
+	return Instance{Name: name, Ip: ip}
+}
+
+func (i Instance) GetEndpoint(port int) Endpoint {
+	url := fmt.Sprintf("tcp://%s:%d", i.Ip, port)
+	return Endpoint{
+		Name: i.Name,
+		URL: url,
+	}
+}
+
+type Tag struct {
+	Key   string
+	Value string
+}
+
+func NewTag(tag string) (Tag, error) {
+	pieces := strings.SplitN(tag, "=", 2)
+	if len(pieces) < 2 {
+		return Tag{}, fmt.Errorf("Invalid tag [%s]. Expected tag=value format", tag)
+	}
+	t := Tag{Key: pieces[0], Value: pieces[1]}
+	log.WithFields(log.Fields{
+		"name": t.Key,
+		"value": t.Value,
+	}).Info("Parsed tag")
+	return t, nil
+}
+
+func (t Tag) String() string {
+	return fmt.Sprintf("%s=%s", t.Key, t.Value)
+}
+
+type Clients struct {
+	EC2Client     ec2iface.EC2API
+}
+
+func NewEC2Client() ec2iface.EC2API {
+	s := session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{Region: aws.String(os.Getenv("AWS_DEFAULT_REGION"))},
+	}))
+	return ec2.New(s)
+}
+
+func initLogging(debug bool) {
 	log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(os.Stderr)
 	log.SetLevel(log.InfoLevel)
+	if debug {
+		log.SetLevel(log.DebugLevel)
+	}
 }
 
-func writeToFile(filepath, content string) error {
-	fo, err := os.Create(filepath)
-	if err != nil {
-		return err
+func getInstances(tag Tag, client ec2iface.EC2API) ([]Instance, error) {
+	params := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("tag:" + tag.Key),
+				Values: aws.StringSlice([]string{tag.Value}),
+			},
+			{
+				Name: aws.String("instance-state-name"),
+				Values: aws.StringSlice([]string{"pending", "running"}),
+			},
+		},
 	}
-	defer fo.Close()
 
-	_, err = fmt.Fprint(fo, content)
-	return err
+	resp, err := client.DescribeInstances(params)
+	if err != nil {
+		return []Instance{}, errors.Wrapf(err, "Describing instances with tag [%s]", tag)
+	}
+
+	instances := []Instance{}
+	for _, r := range resp.Reservations {
+		for _, i := range r.Instances {
+			instances = append(instances, NewInstance(i))
+		}
+	}
+
+	log.WithFields(log.Fields{
+		"num": len(instances),
+		"tag": tag,
+	}).Debug("Fetched instances")
+	return instances, nil
 }
 
-func fetchEndpoints(config *Config) error {
-	instances, err:= getFilteredInstances(config.Tag)
+func writeEndpoints(endpoints []Endpoint, output string) error {
+	b, err := json.Marshal(endpoints)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "Failed to marshal endpoints")
 	}
 
-	endpoints := createEndpoints(instances, config.Docker)
-	endpoints = append(endpoints, &Endpoint{
-		Name: "local",
-		URL: "unix:///var/run/docker.sock",
-	})
-	b, err := json.MarshalIndent(endpoints, "", "  ")
-	if err != nil {
-		return err
-	}
-
-	if config.OutputFile == "" {
+	if output == "" {
 		fmt.Print(string(b))
-	} else {
-		log.WithField("file", config.OutputFile).Debug("Writing to output file")
-		writeToFile(config.OutputFile, string(b))
 	}
 
+	err = ioutil.WriteFile(output, b, 0644)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to write endpoints file to [%s]", output)
+	}
+
+	log.WithFields(log.Fields{
+		"num": len(endpoints),
+		"output": output,
+	}).Info("Written endpoints")
 	return nil
 }
 
-func run(c *cli.Context) {
-	config, err := NewConfig(c)
+func run(c *Config, ec2Client ec2iface.EC2API) {
+	initLogging(c.Debug)
+	log.WithField("version", version).Info("Portainer Endpoints")
+
+	tag, err := NewTag(c.Tag)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.WithField("version", version).Info("Portainer endpoints manager")
-
-	if config.Debug {
-		log.SetLevel(log.DebugLevel)
-	}
-
 	for {
-		time.Sleep(config.Interval)
-		err := fetchEndpoints(config)
+		time.Sleep(c.Interval)
+		instances, err := getInstances(tag, ec2Client)
 		if err != nil {
-			log.WithField("tag", config.Tag).Warnf("Error while fetching endpoints: %s", err)
+			log.Warnf("Error while fetching instances: %s", err)
+			continue
+		}
+
+		endpoints := []Endpoint{{
+			Name: "local",
+			URL:  "unix:///var/run/docker.sock",
+		}}
+		for _, i := range instances {
+			endpoints = append(endpoints, i.GetEndpoint(c.Port))
+		}
+
+		err = writeEndpoints(endpoints, c.Output)
+		if err != nil {
+			log.Warnf("Error while writing endpoints: %s", err)
+			continue
 		}
 	}
 }
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "portainer-endpoints"
-	app.Usage = "Command line tool for generating a json file with Docker endpoints for Portainer"
+	app.Name = "ddns"
+	app.Usage = "Command line tool for dynamically generating domain entries for EC2 instances"
 	app.Version = version
-	app.Action = run
+
 	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:   "debug, d",
-			Usage:  "Enable debug logging",
-			EnvVar: "DEBUG",
-		},
 		cli.StringFlag{
 			Name:   "tag, t",
-			Usage:  "Tag used to filter EC2 instances",
-			EnvVar: "TAG",
+			Usage:  "Tag used to filter EC2 instances. Format tag=value",
+			EnvVar: envPrefix + "TAG",
+		},
+		cli.StringFlag{
+			Name:   "output, o",
+			Usage:  "Path of the output file",
+			EnvVar: envPrefix + "OUTPUT",
 		},
 		cli.IntFlag{
 			Name:   "port, p",
-			Usage:  "Port for the Docker remote API",
-			EnvVar: "PORT",
+			Usage:  "Docker port",
+			Value:  2375,
+			EnvVar: envPrefix + "PORT",
 		},
 		cli.DurationFlag{
 			Name:   "interval, i",
 			Usage:  "Interval for querying the EC2 isntances",
-			EnvVar: "INTERVAL",
+			Value:  30 * time.Second,
+			EnvVar: envPrefix + "INTERVAL",
 		},
-		cli.StringFlag{
-			Name:   "ca-cert",
-			Usage:  "Path to the ca certificate",
-			EnvVar: "CA_CERT",
+		cli.BoolFlag{
+			Name:   "debug, D",
+			Usage:  "Enable debug logging",
+			EnvVar: envPrefix + "DEBUG",
 		},
-		cli.StringFlag{
-			Name:   "cert",
-			Usage:  "Path to the certificate",
-			EnvVar: "CERT",
+	}
+
+	app.Action = func(c *cli.Context) error {
+		run(&Config{
+			Tag:      c.String("tag"),
+			Output:   c.String("output"),
+			Port:     c.Int("port"),
+			Interval: c.Duration("interval"),
+			Debug:    c.Bool("debug"),
 		},
-		cli.StringFlag{
-			Name:   "key",
-			Usage:  "Path to the key",
-			EnvVar: "KEY",
-		},
-		cli.StringFlag{
-			Name:   "output-file, o",
-			Usage:  "Path of the output json file. If unspecified it will write to stdout",
-			EnvVar: "OUTPUT_FILE",
-		},
+			NewEC2Client(),
+		)
+		return nil
 	}
 
 	app.Run(os.Args)
